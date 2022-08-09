7c0b271721fbf73616481a103203cdfca1dc8ce1
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-08-08 23:47:57 -0300

    Add project files.

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription();
+            var student = new Student ("Quiel", "Lima", "12345678912", "quiel@gmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
